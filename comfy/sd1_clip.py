67b248632a95d33117170ba80b4921c884ae8667
Aslan <aslan.xie@gmail.com>
2025-12-09 11:09:04 +0800

    Update sd1_clip.py

--- comfy/sd1_clip.py
+++ comfy/sd1_clip.py
@@ -267,7 +267,7 @@ class SDClipModel(torch.nn.Module, ClipTokenWeightEncoder):
         else:
             intermediate_output = self.layer_idx
 
-        outputs = self.transformer(None, attention_mask_model, embeds=embeds, num_tokens=num_tokens, intermediate_output=intermediate_output, final_layer_norm_intermediate=self.layer_norm_hidden_state, dtype=torch.float32, embeds_info=embeds_info)
+        outputs = self.transformer(None, attention_mask_model, embeds=embeds, num_tokens=num_tokens, intermediate_output=intermediate_output, final_layer_norm_intermediate=self.layer_norm_hidden_state, dtype=torch.bfloat16, embeds_info=embeds_info)
 
         if self.layer == "last":
             z = outputs[0].float()
